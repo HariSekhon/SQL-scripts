04063cb0515e78a62722bb95409978ad99d0489e
Hari Sekhon <harisekhon@gmail.com>
2020-08-06 00:22:57 +0100

    added postgres_index_cardinality.sql

---
+++ postgres_index_cardinality.sql
@@ -0,0 +1,32 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2020-08-06 00:18:01 +0100 (Thu, 06 Aug 2020)
+--
+--  vim:ts=2:sts=2:sw=2:et:filetype=sql
+--
+--  https://github.com/harisekhon/sql
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/harisekhon
+--
+
+-- PostgreSQL index cardinality
+--
+-- Tested on PostgreSQL 12.3
+
+SELECT
+  relname,
+  --relkind,
+  reltuples AS "cardinality (reltuples)",
+  relpages
+FROM
+  pg_class
+WHERE
+  relkind = 'i'
+--    AND
+--  relname LIKE 'someprefix%';
+ORDER BY
+  2 DESC;
