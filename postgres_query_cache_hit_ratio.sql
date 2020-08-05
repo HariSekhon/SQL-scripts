e4ec8e8079e91cdf0b3bb986a0fbe9a529e94a60
Hari Sekhon <harisekhon@gmail.com>
2020-08-06 00:28:45 +0100

    updated postgres_query_cache_hit_ratio.sql

--- postgres_query_cache_hit_ratio.sql
+++ postgres_query_cache_hit_ratio.sql
@@ -29,12 +29,12 @@ SELECT
   shared_blks_read,
   -- using greatest() to avoid divide by zero error, by ensuring we divide by at least 1
     shared_blks_hit /
-    GREATEST((shared_blks_hit + shared_blks_read), 1)::float AS shared_blks_hit_ratio,
+    GREATEST(shared_blks_hit + shared_blks_read, 1)::float AS shared_blks_hit_ratio,
     -- casting divisor to float to avoid getting integer maths returning zeros instead of fractional ratios
   local_blks_hit,
   local_blks_read,
     local_blks_hit /
-    GREATEST((local_blks_hit + local_blks_read), 1)::float AS local_blks_hit_ratio,
+    GREATEST(local_blks_hit + local_blks_read, 1)::float AS local_blks_hit_ratio,
   query
 FROM
   pg_stat_statements
