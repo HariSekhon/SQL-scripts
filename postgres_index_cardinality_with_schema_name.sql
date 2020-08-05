1f36d3d8b62ad9cf04516ccd6c9f6e667564108b
Hari Sekhon <harisekhon@gmail.com>
2020-08-06 00:51:28 +0100

    added postgres_index_cardinality_with_schema_name.sql

---
+++ postgres_index_cardinality_with_schema_name.sql
@@ -0,0 +1,49 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2020-08-06 00:18:01 +0100 (Thu, 06 Aug 2020)
+--
+--  vim:ts=2:sts=2:sw=2:et:filetype=sql
+--
+--  https://github.com/harisekhon/sql
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/harisekhon
+--
+
+-- PostgreSQL index cardinality with schema name
+--
+-- Tested on PostgreSQL 12.3
+
+SELECT
+  schema_name,
+  relname,
+  reltuples AS "cardinality (reltuples)",
+  relpages
+FROM (
+  SELECT
+    pg_catalog.pg_namespace.nspname AS schema_name,
+    relname,
+    reltuples,
+    relpages
+  FROM
+    pg_class
+  JOIN
+    pg_namespace ON relnamespace = pg_namespace.oid
+  WHERE
+    relkind = 'i'
+) t
+WHERE
+  schema_name NOT ILIKE 'pg_%'
+    AND
+  schema_name <> 'information_schema'
+--    AND
+--  relname LIKE 'someprefix%';
+    AND
+  relname NOT ILIKE 'pg_%'
+ORDER BY
+  3 DESC,
+  schema_name,
+  relname;
