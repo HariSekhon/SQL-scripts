41302ec715b1651686e59762d5c19aef238e64c2
Hari Sekhon <harisekhon@gmail.com>
2020-08-06 18:05:50 +0100

    updated postgres_databases_by_size_if_accessible.sql

--- postgres_databases_by_size_if_accessible.sql
+++ postgres_databases_by_size_if_accessible.sql
@@ -18,7 +18,7 @@
 -- Tested on PostgreSQL 12.3
 
 SELECT
-	d.datname AS Name,
+  d.datname AS Name,
   pg_catalog.pg_get_userbyid(d.datdba) AS Owner,
   CASE WHEN pg_catalog.has_database_privilege(d.datname, 'CONNECT')
     THEN pg_catalog.pg_size_pretty(pg_catalog.pg_database_size(d.datname))
