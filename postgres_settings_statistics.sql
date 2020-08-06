0b459a6b55ae10975c9ca6297c109772bee844be
Hari Sekhon <harisekhon@gmail.com>
2020-08-06 18:57:00 +0100

    added postgres_settings_statistics.sql

---
+++ postgres_settings_statistics.sql
@@ -0,0 +1,36 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2020-08-06 18:07:25 +0100 (Thu, 06 Aug 2020)
+--
+--  vim:ts=2:sts=2:sw=2:et:filetype=sql
+--
+--  https://github.com/harisekhon/sql
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/harisekhon
+--
+
+-- Show PostgreSQL statistics collection settings eg. track_activities, track_counts, log_planner_stats, log_statement_stats
+--
+-- Tested on PostgreSQL 12.3
+
+SELECT
+  name,
+  setting,
+  -- category,
+  vartype,
+  short_desc,
+  -- enumvals,
+  source,
+  min_val,
+  max_val,
+  boot_val,
+  reset_val,
+  pending_restart
+FROM
+  pg_settings
+WHERE
+  category ILIKE '%Statistics%';
