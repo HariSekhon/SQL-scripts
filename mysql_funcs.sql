2e84d33ef95d071079ca2ae9df9eb46857104060
Hari Sekhon <harisekhon@gmail.com>
2020-08-06 10:33:10 +0100

    added mysql_funcs.sql

---
+++ mysql_funcs.sql
@@ -0,0 +1,40 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2020-08-06 10:08:00 +0100 (Thu, 06 Aug 2020)
+--
+--  vim:ts=2:sts=2:sw=2:et:filetype=sql
+--
+--  https://github.com/harisekhon/sql
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/harisekhon
+--
+
+-- Common MySQL funcs for quick info
+--
+-- Useful for debugging intended vs actual user auth to fix mysql.user table
+
+-- Tested on MySQL 8.0
+
+-- https://dev.mysql.com/doc/refman/8.0/en/sql-function-reference.html
+
+-- many of these allow not using the brackets but use them for portability between MySQL 5.x and 8.x
+SELECT
+  VERSION(),
+  USER(),   -- client's reported user + host, same as SESSION_USER(), SYSTEM_USER()
+  CURRENT_USER(), -- authenticated user name + host name - this is the one you want for debugging your mysql.user table configuration
+  DATABASE(),  -- SCHEMA()
+  NOW(),
+  CURDATE(),
+  CURTIME(),
+  UTC_DATE(),
+  UTC_TIME(),
+  UTC_TIMESTAMP(),
+  SYSDATE(),  -- returns date of function completion
+  -- PS_CURRENT_THREAD_ID(),  -- MySQL 8.0.16+
+  UUID_SHORT(), -- integer
+  UUID()        -- alnum
+;
