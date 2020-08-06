4e3fe89e28bbe2e9b53745da969ab97086fd94e7
Hari Sekhon <harisekhon@gmail.com>
2020-08-06 18:42:49 +0100

    added postgres_settings_replication.sql

---
+++ postgres_settings_replication.sql
@@ -0,0 +1,36 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2020-08-06 18:07:25 +0100 (Thu, 06 Aug 2020)
+--
+--  vim:ts=2:sts=2:sw=2:et:filetype=sql
+--
+--  https://github.com/harisekhon/sql
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/harisekhon
+--
+
+-- Show PostgreSQL Replication settings eg. hot_standby, max_replication_slots, max_wal_senders etc.
+--
+-- Tested on PostgreSQL 12.3
+
+SELECT
+  name,
+  setting,
+  -- category,
+  short_desc,
+  vartype,
+  -- enumvals,
+  source,
+  min_val,
+  max_val,
+  boot_val,
+  reset_val,
+  pending_restart
+FROM
+  pg_settings
+WHERE
+  category ILIKE '%Replication%';
