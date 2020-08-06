505bef52e2c981b9037798ed6f18f8f74bbe9a1c
Hari Sekhon <harisekhon@gmail.com>
2020-08-06 11:46:25 +0100

    added postgres_recovery.sql

---
+++ postgres_recovery.sql
@@ -0,0 +1,29 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2020-08-06 11:36:42 +0100 (Thu, 06 Aug 2020)
+--
+--  vim:ts=2:sts=2:sw=2:et:filetype=sql
+--
+--  https://github.com/harisekhon/sql
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/harisekhon
+--
+
+-- PostgreSQL recovery info
+--
+-- Tested on PostgreSQL 12.3
+
+SELECT
+  pg_is_in_backup(),
+  pg_is_in_recovery(),
+  pg_backup_start_time(),
+  -- the following recovery control functions can only be executed during recovery - to get just the above use postgres_funcs.sql
+  ( CASE WHEN pg_is_in_recovery() THEN pg_is_wal_replay_paused() END)       AS "pg_is_wal_replay_paused()",
+  ( CASE WHEN pg_is_in_recovery() THEN pg_last_wal_receive_lsn() END)       AS "pg_last_wal_receive_lsn()",
+  ( CASE WHEN pg_is_in_recovery() THEN pg_last_wal_replay_lsn() END)        AS "pg_last_wal_replay_lsn()",
+  ( CASE WHEN pg_is_in_recovery() THEN pg_last_xact_replay_timestamp() END) AS "pg_last_xact_replay_timestamp()"
+;
