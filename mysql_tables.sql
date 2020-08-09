6569fa56c335a100c16235ae02a6056799dc0ff6
Hari Sekhon <harisekhon@gmail.com>
2020-08-09 09:58:13 +0100

    updated mysql_tables.sql

--- mysql_tables.sql
+++ mysql_tables.sql
@@ -15,7 +15,7 @@
 
 -- MySQL tables across schemas (useful for finding tables)
 --
--- Tested on MySQL 5.5, 5.6, 5.7, 8.0 and MariaDB 5.5, 10.5
+-- Tested on MySQL 5.5, 5.6, 5.7, 8.0 and MariaDB 5.5, 10.0 - 10.5
 
 SELECT
   table_schema,
@@ -35,7 +35,7 @@ SELECT
 FROM
   information_schema.tables
 WHERE
-  table_type <> 'VIEW'
+  table_type NOT LIKE '%VIEW%'
 ORDER BY
   table_schema,
   table_name;
