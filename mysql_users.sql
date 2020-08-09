1b61187e9da7061dc546a07d254fa3f6a9bfed6a
Hari Sekhon <harisekhon@gmail.com>
2020-08-09 09:59:43 +0100

    updated mysql_users.sql

--- mysql_users.sql
+++ mysql_users.sql
@@ -17,7 +17,7 @@
 --
 -- Tested on MySQL 8.0
 --
--- some fields doesn't exist on MySQL < 8.0 and MariaDB 10.5, must use mysql_users_pre8.sql instead
+-- some fields doesn't exist on MySQL < 8.0 and MariaDB 10.0 - 10.5, must use mysql_users_pre8.sql instead
 
 SELECT
   host,
