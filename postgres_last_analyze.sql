915c662e345cca95cb145be6fe24f63ac39be873
Hari Sekhon <harisekhon@gmail.com>
2020-08-10 19:44:00 +0100

    updated postgres_last_analyze.sql

--- postgres_last_analyze.sql
+++ postgres_last_analyze.sql
@@ -15,16 +15,22 @@
 
 -- PostgreSQL Analyze info for tables with rows modified since last analyze
 --
--- Tested on PostgreSQL 12.3
+-- Requires PostgreSQL 9.4+ (see postgres_last_analyze_pre94.sql for earlier versions)
+--
+-- Tested on PostgreSQL 9.4+, 10.x, 11.x, 12.x
 
 SELECT
   schemaname,
   relname,
+  -- not available on PostgreSQL <= 9.3
   n_mod_since_analyze,
   last_analyze,
   last_autoanalyze,
+  -- not available on PostgreSQL <= 9.0
   analyze_count,
   autoanalyze_count
 FROM pg_stat_user_tables
 ORDER BY
-  n_mod_since_analyze DESC;
+  n_mod_since_analyze DESC,
+  last_analyze DESC,
+  last_autoanalyze DESC;
