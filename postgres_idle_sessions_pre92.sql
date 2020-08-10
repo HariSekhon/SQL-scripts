c532fef5e66866399978c30ec269046cc434a1b0
Hari Sekhon <harisekhon@gmail.com>
2020-08-10 15:51:17 +0100

    added postgres_idle_sessions_pre92.sql

---
+++ postgres_idle_sessions_pre92.sql
@@ -0,0 +1,53 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2020-08-05 18:34:44 +0100 (Wed, 05 Aug 2020)
+--
+--  vim:ts=2:sts=2:sw=2:et:filetype=sql
+--
+--  https://github.com/harisekhon/sql
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/harisekhon
+--
+
+-- List PostgreSQL stale connections that have been idle > 10 mins
+--
+-- Can be used to kill connections using the procpids returned, eg.
+--
+-- SELECT pg_terminate_backend(procpid)
+--
+-- Requires PostgreSQL <= 9.1
+--
+-- Tested on PostgreSQL 8.4, 9.0, 9.1
+
+SELECT
+  rank() over (partition by client_addr order by backend_start ASC) as rank,
+  procpid,
+  backend_start,
+  query_start,
+  waiting,
+  datname,
+  usename,
+  client_addr
+FROM
+  pg_stat_activity
+WHERE
+  -- don't kill yourself
+  procpid <> pg_backend_pid()
+  --  AND
+  -- don't kill your admin tools
+  --application_name !~ '(?:psql)|(?:pgAdmin.+)'
+  --  AND
+  --usename not in ('postgres')
+    AND
+  current_query in ('')
+    AND
+  (
+    (current_timestamp - query_start) > interval '10 minutes'
+      OR
+    (query_start IS NULL AND (current_timestamp - backend_start) > interval '10 minutes')
+  )
+;
