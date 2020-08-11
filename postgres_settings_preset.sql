256e3949d83baf8b96dc03cb8fdf991c92f302c2
Hari Sekhon <harisekhon@gmail.com>
2020-08-11 12:55:42 +0100

    updated postgres_settings_preset.sql

--- postgres_settings_preset.sql
+++ postgres_settings_preset.sql
@@ -15,7 +15,9 @@
 
 -- Show PostgreSQL preset options settings eg. block_size, wal_block_size, data_checksums, server_version, server_version_num, ssl_library
 --
--- Tested on PostgreSQL 12.3
+-- Requires PostgreSQL 9.5+
+--
+-- Tested on PostgreSQL 9.5, 9.6, 10.x 11.x, 12.x
 
 -- https://www.postgresql.org/docs/12/runtime-config-preset.html
 
