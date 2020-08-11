93bef39b73bdb916d254ee85cefb3ad61c8d7a46
Hari Sekhon <harisekhon@gmail.com>
2020-08-11 11:41:52 +0100

    updated postgres_sequences_restart_all.sql

--- postgres_sequences_restart_all.sql
+++ postgres_sequences_restart_all.sql
@@ -15,7 +15,7 @@
 
 -- Restarts all PostgreSQL sequences
 --
--- Tested on PostgreSQL 12.3
+-- Tested on PostgreSQL 8.4, 9.x, 10.x, 11.x, 12.x
 
 SELECT
   'ALTER SEQUENCE ' || relname || ' RESTART;'
