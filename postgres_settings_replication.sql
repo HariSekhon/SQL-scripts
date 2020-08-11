abf75fed53b28da53f875f9a3cdc29090e4e8678
Hari Sekhon <harisekhon@gmail.com>
2020-08-11 12:56:06 +0100

    updated postgres_settings_replication.sql

--- postgres_settings_replication.sql
+++ postgres_settings_replication.sql
@@ -15,7 +15,9 @@
 
 -- Show PostgreSQL Replication settings eg. hot_standby, max_replication_slots, max_wal_senders etc.
 --
--- Tested on PostgreSQL 12.3
+-- Requires PostgreSQL 9.5+
+--
+-- Tested on PostgreSQL 9.5, 9.6, 10.x 11.x, 12.x
 
 -- https://www.postgresql.org/docs/12/runtime-config-replication.html
 
