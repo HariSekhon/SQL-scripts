c9e2037c4eba6d2812ddf195852a2b538bba71be
Hari Sekhon <harisekhon@gmail.com>
2020-08-11 12:15:49 +0100

    updated postgres_session_state_count.sql

--- postgres_session_state_count.sql
+++ postgres_session_state_count.sql
@@ -15,7 +15,9 @@
 
 -- PostgreSQL count of sessions in each state
 --
--- Tested on PostgreSQL 12.3
+-- Requires PostgreSQL 9.2+
+--
+-- Tested on PostgreSQL 9.2+, 10.x, 11.x, 12.x
 
 SELECT
   count(1),
