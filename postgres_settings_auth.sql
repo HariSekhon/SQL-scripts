0f8d4cadf648d81242156be3159ef4ab72c0887d
Hari Sekhon <harisekhon@gmail.com>
2020-08-11 12:28:49 +0100

    updated postgres_settings_auth.sql

--- postgres_settings_auth.sql
+++ postgres_settings_auth.sql
@@ -15,7 +15,9 @@
 
 -- Show PostgreSQL authentication settings eg. password_encryption, krb_server_keyfile
 --
--- Tested on PostgreSQL 12.3
+-- Requires PostgreSQL 9.5+
+--
+-- Tested on PostgreSQL 9.5, 9.6, 10.x 11.x, 12.x
 
 -- https://www.postgresql.org/docs/12/runtime-config-connection.html#RUNTIME-CONFIG-CONNECTION-AUTHENTICATION
 
