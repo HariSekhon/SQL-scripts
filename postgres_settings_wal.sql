48b0a1e2eb2f47dbbd8183e37c61b6baed7bb7bb
Hari Sekhon <harisekhon@gmail.com>
2020-08-11 12:56:55 +0100

    updated postgres_settings_wal.sql

--- postgres_settings_wal.sql
+++ postgres_settings_wal.sql
@@ -15,7 +15,9 @@
 
 -- Show PostgreSQL WAL settings eg. fsync, wal_level, wal_buffers, wal_sync_method, synchronous_commit, checkpoint_timeout, archive_mode etc.
 --
--- Tested on PostgreSQL 12.3
+-- Requires PostgreSQL 9.5+
+--
+-- Tested on PostgreSQL 9.5, 9.6, 10.x 11.x, 12.x
 
 -- https://www.postgresql.org/docs/12/runtime-config-wal.html
 
