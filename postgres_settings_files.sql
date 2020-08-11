c1ab7e314d8913d32c882a158cd62f0d435c8a65
Hari Sekhon <harisekhon@gmail.com>
2020-08-11 12:54:41 +0100

    updated postgres_settings_files.sql

--- postgres_settings_files.sql
+++ postgres_settings_files.sql
@@ -15,7 +15,9 @@
 
 -- Show PostgreSQL file location settings eg. config_file, hba_file, data_directory
 --
--- Tested on PostgreSQL 12.3
+-- Requires PostgreSQL 9.5+
+--
+-- Tested on PostgreSQL 9.5, 9.6, 10.x 11.x, 12.x
 
 -- https://www.postgresql.org/docs/12/runtime-config-file-locations.html
 
