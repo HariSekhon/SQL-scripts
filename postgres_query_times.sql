6f99b0b3aff81442a7bc8cbd82a510296a538ffd
Hari Sekhon <harisekhon@gmail.com>
2020-08-11 10:59:11 +0100

    updated postgres_query_times.sql

--- postgres_query_times.sql
+++ postgres_query_times.sql
@@ -15,7 +15,9 @@
 
 -- PostgreSQL query times from pg_stat_statements
 --
--- Tested on PostgreSQL 12.3
+-- Requires PostgreSQL 9.5+
+--
+-- Tested on PostgreSQL 9.5+, 10.x, 11.x, 12.x
 
 -- postgresql.conf needs before start:
 -- shared_preload_libraries = 'pg_stat_statements'
