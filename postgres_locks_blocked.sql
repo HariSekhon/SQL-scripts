b96565c88854085546ac0684c064cc377f00ec34
Hari Sekhon <harisekhon@gmail.com>
2020-08-17 23:40:42 +0100

    updated postgres_locks_blocked.sql

--- postgres_locks_blocked.sql
+++ postgres_locks_blocked.sql
@@ -22,7 +22,7 @@
 -- https://wiki.postgresql.org/wiki/Lock_Monitoring
 
 SELECT
-	blocked_locks.pid         AS blocked_pid,
+  blocked_locks.pid         AS blocked_pid,
   blocked_activity.usename  AS blocked_user,
   blocking_locks.pid        AS blocking_pid,
   blocking_activity.usename AS blocking_user,
@@ -37,16 +37,16 @@ ON
 JOIN
   pg_catalog.pg_locks AS blocking_locks
 ON
-	blocking_locks.locktype = blocked_locks.locktype
-  AND blocking_locks.database IS NOT DISTINCT FROM blocked_locks.database
-  AND blocking_locks.relation IS NOT DISTINCT FROM blocked_locks.relation
-  AND blocking_locks.page IS NOT DISTINCT FROM blocked_locks.page
-  AND blocking_locks.tuple IS NOT DISTINCT FROM blocked_locks.tuple
-  AND blocking_locks.virtualxid IS NOT DISTINCT FROM blocked_locks.virtualxid
-  AND blocking_locks.transactionid IS NOT DISTINCT FROM blocked_locks.transactionid
-  AND blocking_locks.classid IS NOT DISTINCT FROM blocked_locks.classid
-  AND blocking_locks.objid IS NOT DISTINCT FROM blocked_locks.objid
-  AND blocking_locks.objsubid IS NOT DISTINCT FROM blocked_locks.objsubid
+  blocking_locks.locktype = blocked_locks.locktype
+  AND blocking_locks.database       IS NOT DISTINCT FROM blocked_locks.database
+  AND blocking_locks.relation       IS NOT DISTINCT FROM blocked_locks.relation
+  AND blocking_locks.page           IS NOT DISTINCT FROM blocked_locks.page
+  AND blocking_locks.tuple          IS NOT DISTINCT FROM blocked_locks.tuple
+  AND blocking_locks.virtualxid     IS NOT DISTINCT FROM blocked_locks.virtualxid
+  AND blocking_locks.transactionid  IS NOT DISTINCT FROM blocked_locks.transactionid
+  AND blocking_locks.classid        IS NOT DISTINCT FROM blocked_locks.classid
+  AND blocking_locks.objid          IS NOT DISTINCT FROM blocked_locks.objid
+  AND blocking_locks.objsubid       IS NOT DISTINCT FROM blocked_locks.objsubid
   AND blocking_locks.pid != blocked_locks.pid
 JOIN
   pg_catalog.pg_stat_activity blocking_activity
