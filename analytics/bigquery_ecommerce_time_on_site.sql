27d47e8468a1ca53d37585d2d3f9cfaa300b953c
Hari Sekhon <harisekhon@gmail.com>
2020-09-06 09:51:56 +0100

    added bigquery_ecommerce_time_on_site.sql

---
+++ analytics/bigquery_ecommerce_time_on_site.sql
@@ -0,0 +1,45 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2020-09-06 09:49:38 +0100 (Sun, 06 Sep 2020)
+--
+--  vim:ts=2:sts=2:sw=2:et:filetype=sql
+--
+--  https://github.com/harisekhon/sql
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/harisekhon
+--
+
+SELECT
+  * EXCEPT(fullVisitorId)
+FROM
+
+  # features
+  (SELECT
+    fullVisitorId,
+    IFNULL(totals.bounces, 0) AS bounces,
+    IFNULL(totals.timeOnSite, 0) AS time_on_site
+  FROM
+    `data-to-insights.ecommerce.web_analytics`
+  WHERE
+    totals.newVisits = 1)
+  JOIN
+  (SELECT
+    fullvisitorid,
+    IF(
+      COUNTIF(
+        totals.transactions > 0
+          AND
+        totals.newVisits IS NULL
+      ) > 0, 1, 0
+    ) AS will_buy_on_return_visit
+  FROM
+      `data-to-insights.ecommerce.web_analytics`
+  GROUP BY fullvisitorid)
+  USING (fullVisitorId)
+ORDER BY
+  time_on_site DESC
+LIMIT 10;
