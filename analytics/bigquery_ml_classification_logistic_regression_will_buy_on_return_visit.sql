ae50a3ddce90db7f347e3b91dea655880bd68b4e
Hari Sekhon <harisekhon@gmail.com>
2020-09-06 09:54:40 +0100

    added bigquery_ml_classification_logistic_regression_will_buy_on_return_visit.sql

---
+++ analytics/bigquery_ml_classification_logistic_regression_will_buy_on_return_visit.sql
@@ -0,0 +1,47 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2020-09-06 09:54:10 +0100 (Sun, 06 Sep 2020)
+--
+--  vim:ts=2:sts=2:sw=2:et:filetype=sql
+--
+--  https://github.com/harisekhon/sql
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/harisekhon
+--
+
+CREATE OR REPLACE MODEL `ecommerce.classification_model`
+OPTIONS
+(
+model_type='logistic_reg',
+labels = ['will_buy_on_return_visit']
+)
+AS
+
+#standardSQL
+SELECT
+  * EXCEPT(fullVisitorId)
+FROM
+
+  # features
+  (SELECT
+    fullVisitorId,
+    IFNULL(totals.bounces, 0) AS bounces,
+    IFNULL(totals.timeOnSite, 0) AS time_on_site
+  FROM
+    `data-to-insights.ecommerce.web_analytics`
+  WHERE
+    totals.newVisits = 1
+    AND date BETWEEN '20160801' AND '20170430') # train on first 9 months
+  JOIN
+  (SELECT
+    fullvisitorid,
+    IF(COUNTIF(totals.transactions > 0 AND totals.newVisits IS NULL) > 0, 1, 0) AS will_buy_on_return_visit
+  FROM
+      `data-to-insights.ecommerce.web_analytics`
+  GROUP BY fullvisitorid)
+  USING (fullVisitorId)
+;
