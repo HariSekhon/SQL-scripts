0a8aba3c7341c52ec516aa5e96d214036ab3a98f
Hari Sekhon <harisekhon@gmail.com>
2020-09-06 09:38:15 +0100

    added bigquery_ecommerce_top_5_products.sql

---
+++ analytics/bigquery_ecommerce_top_5_products.sql
@@ -0,0 +1,28 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2020-09-06 09:36:31 +0100 (Sun, 06 Sep 2020)
+--
+--  vim:ts=2:sts=2:sw=2:et:filetype=sql
+--
+--  https://github.com/harisekhon/sql
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/harisekhon
+--
+
+SELECT
+  p.v2ProductName,
+  p.v2ProductCategory,
+  SUM(p.productQuantity) AS units_sold,
+  ROUND(SUM(p.localProductRevenue/1000000),2) AS revenue
+FROM
+	`data-to-insights.ecommerce.web_analytics`,
+UNNEST(hits) AS h,
+UNNEST(h.product) AS p
+GROUP BY 1, 2
+ORDER BY
+	revenue DESC
+LIMIT 5;
