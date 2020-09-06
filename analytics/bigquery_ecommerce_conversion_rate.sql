29504e3ad5837bb183a91383604f8e192804697a
Hari Sekhon <harisekhon@gmail.com>
2020-09-06 09:36:18 +0100

    added bigquery_ecommerce_conversion_rate.sql

---
+++ analytics/bigquery_ecommerce_conversion_rate.sql
@@ -0,0 +1,37 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2020-09-06 09:22:11 +0100 (Sun, 06 Sep 2020)
+--
+--  vim:ts=2:sts=2:sw=2:et:filetype=sql
+--
+--  https://github.com/harisekhon/sql
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/harisekhon
+--
+
+WITH visitors AS(
+	SELECT
+	COUNT(DISTINCT fullVisitorId) AS total_visitors
+	FROM `data-to-insights.ecommerce.web_analytics`
+),
+
+purchasers AS(
+	SELECT
+	COUNT(DISTINCT fullVisitorId) AS total_purchasers
+	FROM `data-to-insights.ecommerce.web_analytics`
+	WHERE totals.transactions IS NOT NULL
+)
+
+SELECT
+  total_visitors,
+  total_purchasers,
+  total_purchasers / total_visitors AS conversion_rate,
+  ROUND(total_purchasers / total_visitors * 100, 2) AS conversion_percentage
+FROM
+	visitors,
+	purchasers
+;
