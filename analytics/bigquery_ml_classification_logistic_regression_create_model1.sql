8c866d9ca5e9e8bec9014c21f7ec3267b5f914a2
Hari Sekhon <harisekhon@gmail.com>
2020-09-07 11:55:43 +0100

    updated bigquery_ml_classification_logistic_regression_create_model1.sql

--- analytics/bigquery_ml_classification_logistic_regression_create_model1.sql
+++ analytics/bigquery_ml_classification_logistic_regression_create_model1.sql
@@ -15,13 +15,13 @@
 
 -- ran for 2 minutes to build the trained model
 --
--- This statement created a new model named <project>:ecommerce.classification_model.
+-- This statement created a new model named <project>:ecommerce.classification_model1
 --
 -- - click "Go to model" on the far right of the line
 
 -- this model is weak in feature selection - see model2 for a much better more realistic model
 
-CREATE OR REPLACE MODEL `ecommerce.classification_model`
+CREATE OR REPLACE MODEL `ecommerce.classification_model1`
 OPTIONS
 (
 model_type='logistic_reg',
@@ -29,27 +29,36 @@ labels = ['will_buy_on_return_visit']
 )
 AS
 
-#standardSQL
+-- training data provided below via SELECT statement
+
 SELECT
   * EXCEPT(fullVisitorId)
 FROM
 
-  # features
-  (SELECT
-    fullVisitorId,
-    IFNULL(totals.bounces, 0) AS bounces,
-    IFNULL(totals.timeOnSite, 0) AS time_on_site
-  FROM
-    `data-to-insights.ecommerce.web_analytics`
-  WHERE
-    totals.newVisits = 1
-    AND date BETWEEN '20160801' AND '20170430') # train on first 9 months only so we can test on the rest of the data
-  JOIN
-  (SELECT
-    fullvisitorid,
-    IF(COUNTIF(totals.transactions > 0 AND totals.newVisits IS NULL) > 0, 1, 0) AS will_buy_on_return_visit
-  FROM
+  -- features
+  (
+    SELECT
+      fullVisitorId,
+      IFNULL(totals.bounces, 0) AS bounces,
+      IFNULL(totals.timeOnSite, 0) AS time_on_site
+    FROM
       `data-to-insights.ecommerce.web_analytics`
-  GROUP BY fullvisitorid)
-  USING (fullVisitorId)
+    WHERE
+      totals.newVisits = 1
+      -- train on subset of the data so we can test on the rest of the data
+      AND date BETWEEN '20160801' AND '20170430'
+  )
+  JOIN
+  (
+    SELECT
+      fullvisitorid,
+      -- the label of answers to train on
+      IF(COUNTIF(totals.transactions > 0 AND totals.newVisits IS NULL) > 0, 1, 0) AS will_buy_on_return_visit
+    FROM
+      `data-to-insights.ecommerce.web_analytics`
+    GROUP BY
+      fullvisitorid
+  )
+  USING
+    (fullVisitorId)
 ;
