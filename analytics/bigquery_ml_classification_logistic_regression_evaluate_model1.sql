a09a011a530ab69526dcae3e0c3f0cc6412e1b78
Hari Sekhon <harisekhon@gmail.com>
2020-09-07 11:59:22 +0100

    updated bigquery_ml_classification_logistic_regression_evaluate_model1.sql

--- analytics/bigquery_ml_classification_logistic_regression_evaluate_model1.sql
+++ analytics/bigquery_ml_classification_logistic_regression_evaluate_model1.sql
@@ -28,29 +28,38 @@ SELECT
   precision,
   recall
 FROM
-  ML.EVALUATE(MODEL ecommerce.classification_model,  (
+  ML.EVALUATE(
+    MODEL ecommerce.classification_model1, (
 
-SELECT
-  * EXCEPT(fullVisitorId)
-FROM
+      -- the test data is supplied below via a SELECT query for the model to compare its predictions to (notice this is non-overlapping with the training data)
 
-  # features
-  (SELECT
-    fullVisitorId,
-    IFNULL(totals.bounces, 0) AS bounces,
-    IFNULL(totals.timeOnSite, 0) AS time_on_site
-  FROM
-    `data-to-insights.ecommerce.web_analytics`
-  WHERE
-    totals.newVisits = 1
-    AND date BETWEEN '20170501' AND '20170630') # eval on 2 months
-  JOIN
-  (SELECT
-    fullvisitorid,
-    IF(COUNTIF(totals.transactions > 0 AND totals.newVisits IS NULL) > 0, 1, 0) AS will_buy_on_return_visit
-  FROM
-      `data-to-insights.ecommerce.web_analytics`
-  GROUP BY fullvisitorid)
-  USING (fullVisitorId)
+      SELECT
+        * EXCEPT(fullVisitorId)
+      FROM
 
-));
+        -- features
+        (
+          SELECT
+            fullVisitorId,
+            IFNULL(totals.bounces, 0) AS bounces,
+            IFNULL(totals.timeOnSite, 0) AS time_on_site
+          FROM
+            `data-to-insights.ecommerce.web_analytics`
+          WHERE
+            totals.newVisits = 1
+            -- notice this time period doesn't overlap with the training data
+            AND date BETWEEN '20170501' AND '20170630'
+        )
+        JOIN
+        (
+          SELECT
+            fullvisitorid,
+            IF(COUNTIF(totals.transactions > 0 AND totals.newVisits IS NULL) > 0, 1, 0) AS will_buy_on_return_visit
+          FROM
+            `data-to-insights.ecommerce.web_analytics`
+          GROUP BY
+            fullvisitorid
+        )
+        USING (fullVisitorId)
+    )
+  );
