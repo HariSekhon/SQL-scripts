6c8b24efaadb7af0c62a758dcecf27399c993e2f
Hari Sekhon <harisekhon@gmail.com>
2020-09-07 12:24:19 +0100

    updated bigquery_ml_classification_logistic_regression_create_model2.sql

--- analytics/bigquery_ml_classification_logistic_regression_create_model2.sql
+++ analytics/bigquery_ml_classification_logistic_regression_create_model2.sql
@@ -18,63 +18,71 @@
 -- better feature selection than model 1
 
 CREATE OR REPLACE MODEL `ecommerce.classification_model_2`
-OPTIONS
-  (model_type='logistic_reg', labels = ['will_buy_on_return_visit']) AS
+OPTIONS (
+  model_type='logistic_reg',
+  labels = ['will_buy_on_return_visit']
+) AS
+
+-- training data provided below via SELECT statements
 
 WITH all_visitor_stats AS (
-SELECT
-  fullvisitorid,
-  IF(COUNTIF(totals.transactions > 0 AND totals.newVisits IS NULL) > 0, 1, 0) AS will_buy_on_return_visit
-  FROM `data-to-insights.ecommerce.web_analytics`
-  GROUP BY fullvisitorid
+  SELECT
+    fullvisitorid,
+    IF(COUNTIF(totals.transactions > 0 AND totals.newVisits IS NULL) > 0, 1, 0) AS will_buy_on_return_visit
+    FROM `data-to-insights.ecommerce.web_analytics`
+    GROUP BY fullvisitorid
 )
 
-# add in new features
+-- select features
 SELECT * EXCEPT(unique_session_id) FROM (
 
   SELECT
-      CONCAT(fullvisitorid, CAST(visitId AS STRING)) AS unique_session_id,
+    CONCAT(fullvisitorid, CAST(visitId AS STRING)) AS unique_session_id,
 
-      # labels
-      will_buy_on_return_visit,
+    -- labels
+    will_buy_on_return_visit,
 
-      MAX(CAST(h.eCommerceAction.action_type AS INT64)) AS latest_ecommerce_progress,
+    MAX(CAST(h.eCommerceAction.action_type AS INT64)) AS latest_ecommerce_progress,
 
-      # behavior on the site
-      IFNULL(totals.bounces, 0) AS bounces,
-      IFNULL(totals.timeOnSite, 0) AS time_on_site,
-      totals.pageviews,
+    -- behavior on the site
+    IFNULL(totals.bounces, 0) AS bounces,
+    IFNULL(totals.timeOnSite, 0) AS time_on_site,
+    totals.pageviews,
 
-      # where the visitor came from
-      trafficSource.source,
-      trafficSource.medium,
-      channelGrouping,
+    -- where the visitor came from
+    trafficSource.source,
+    trafficSource.medium,
+    channelGrouping,
 
-      # mobile or desktop
-      device.deviceCategory,
+    -- mobile or desktop
+    device.deviceCategory,
 
-      # geographic
-      IFNULL(geoNetwork.country, "") AS country
+    -- geographic
+    IFNULL(geoNetwork.country, "") AS country
 
   FROM `data-to-insights.ecommerce.web_analytics`,
-     UNNEST(hits) AS h
+    UNNEST(hits) AS h
 
-    JOIN all_visitor_stats USING(fullvisitorid)
+  JOIN
+    all_visitor_stats
+  USING
+    (fullvisitorid)
 
   WHERE 1=1
-    # only predict for new visits
+    -- only predict for new visits
     AND totals.newVisits = 1
-    AND date BETWEEN '20160801' AND '20170430' # train 9 months
+    -- train on subset of the data so we can test on the rest of the data
+    AND date BETWEEN '20160801' AND '20170430'
 
   GROUP BY
-  unique_session_id,
-  will_buy_on_return_visit,
-  bounces,
-  time_on_site,
-  totals.pageviews,
-  trafficSource.source,
-  trafficSource.medium,
-  channelGrouping,
-  device.deviceCategory,
-  country
+    unique_session_id,
+    will_buy_on_return_visit,
+    bounces,
+    time_on_site,
+    totals.pageviews,
+    trafficSource.source,
+    trafficSource.medium,
+    channelGrouping,
+    device.deviceCategory,
+    country
 );
