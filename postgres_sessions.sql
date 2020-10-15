83a65e6a1c84993f177becc08d62153a2a6b7043
Hari Sekhon <harisekhon@gmail.com>
2020-10-15 18:58:25 +0100

    added postgres_sessions.sql

---
+++ postgres_sessions.sql
@@ -0,0 +1,35 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2020-08-06 00:23:40 +0100 (Thu, 06 Aug 2020)
+--
+--  vim:ts=2:sts=2:sw=2:et:filetype=sql
+--
+--  https://github.com/harisekhon/sql
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/harisekhon
+--
+
+-- PostgreSQL count of sessions in each state
+--
+-- Requires PostgreSQL 9.2+
+--
+-- Tested on PostgreSQL 9.2+, 10.x, 11.x, 12.x
+
+SELECT
+  pid,
+  usename,
+  client_addr,
+  client_hostname,
+  client_port,
+  backend_start,
+  query_start,
+  state,
+  backend_type
+FROM
+  pg_stat_activity
+ORDER BY
+  backend_type;
