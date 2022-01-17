88eab5ff7761d94d05b0792cee5c822094e2a0e3
Hari Sekhon <harisekhon@gmail.com>
2022-01-17 17:30:41 +0000

    updated bigquery_ecommerce_top_5_products.sql

--- analytics/bigquery_ecommerce_top_5_products.sql
+++ analytics/bigquery_ecommerce_top_5_products.sql
@@ -19,10 +19,10 @@ SELECT
   SUM(p.productQuantity) AS units_sold,
   ROUND(SUM(p.localProductRevenue/1000000),2) AS revenue
 FROM
-	`data-to-insights.ecommerce.web_analytics`,
+  `data-to-insights.ecommerce.web_analytics`,
 UNNEST(hits) AS h,
 UNNEST(h.product) AS p
 GROUP BY 1, 2
 ORDER BY
-	revenue DESC
+  revenue DESC
 LIMIT 5;
