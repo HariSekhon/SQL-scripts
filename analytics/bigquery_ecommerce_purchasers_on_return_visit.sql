99b7256b5a4c3687bbf6e6ed790a1ba4a8e8b00b
Hari Sekhon <harisekhon@gmail.com>
2022-01-17 17:30:47 +0000

    updated bigquery_ecommerce_purchasers_on_return_visit.sql

--- analytics/bigquery_ecommerce_purchasers_on_return_visit.sql
+++ analytics/bigquery_ecommerce_purchasers_on_return_visit.sql
@@ -18,20 +18,20 @@ WITH all_visitor_stats AS (
 SELECT
   fullvisitorid,
   IF(
-  	COUNTIF(totals.transactions > 0
-		AND
+    COUNTIF(totals.transactions > 0
+    AND
     totals.newVisits IS NULL) > 0, 1, 0
-  	) AS will_buy_on_return_visit
+    ) AS will_buy_on_return_visit
   FROM
-		`data-to-insights.ecommerce.web_analytics`
+    `data-to-insights.ecommerce.web_analytics`
   GROUP BY
-		fullvisitorid
+    fullvisitorid
 )
 
 SELECT
   COUNT(DISTINCT fullvisitorid) AS total_visitors,
   will_buy_on_return_visit
 FROM
-	all_visitor_stats
+  all_visitor_stats
 GROUP BY
-	will_buy_on_return_visit;
+  will_buy_on_return_visit;
