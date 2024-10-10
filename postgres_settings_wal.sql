03d68a190acf5360bffe1371fe2dec9fd43f7654
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:50:25 +0300

    updated postgres_settings_wal.sql

--- postgres_settings_wal.sql
+++ postgres_settings_wal.sql
@@ -24,25 +24,25 @@
 -- https://www.postgresql.org/docs/12/runtime-config-wal.html
 
 SELECT
-  name,
-  setting,
-  -- category,
-  vartype,
-  short_desc,
-  -- enumvals,
-  source,
-  min_val,
-  max_val,
-  boot_val,
-  reset_val,
-  -- not available on PostgreSQL < 9.5
-  pending_restart
+    name,
+    setting,
+    -- category,
+    vartype,
+    short_desc,
+    -- enumvals,
+    source,
+    min_val,
+    max_val,
+    boot_val,
+    reset_val,
+    -- not available on PostgreSQL < 9.5
+    pending_restart
 FROM
-  pg_settings
+    pg_settings
 WHERE
-  -- Write-Ahead Log / Settings
-  -- Write-Ahead Log / Recovery Target
-  -- Write-Ahead Log / Checkpoints
-  -- Write-Ahead Log / Archive Recovery
-  -- Write-Ahead Log / Archiving
-  category ILIKE '%Write-Ahead Log%';
+    -- Write-Ahead Log / Settings
+    -- Write-Ahead Log / Recovery Target
+    -- Write-Ahead Log / Checkpoints
+    -- Write-Ahead Log / Archive Recovery
+    -- Write-Ahead Log / Archiving
+    category ILIKE '%Write-Ahead Log%';
