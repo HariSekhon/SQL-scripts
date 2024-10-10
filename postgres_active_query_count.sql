e6b730706ba4fd26544ade05a8459b77949d4d9d
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:37:59 +0300

    updated postgres_active_query_count.sql

--- postgres_active_query_count.sql
+++ postgres_active_query_count.sql
@@ -2,7 +2,7 @@
 --  Author: Hari Sekhon
 --  Date: 2020-08-05 15:33:36 +0100 (Wed, 05 Aug 2020)
 --
---  vim:ts=2:sts=2:sw=2:et:filetype=sql
+--  vim:ts=4:sts=4:sw=4:et:filetype=sql
 --
 --  https://github.com/HariSekhon/SQL-scripts
 --
