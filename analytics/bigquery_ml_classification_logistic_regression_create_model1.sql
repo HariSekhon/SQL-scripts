eaac4b82b8a42218616fd836fdc60254d4a9ee2a
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:52:21 +0300

    updated bigquery_ml_classification_logistic_regression_create_model1.sql

--- analytics/bigquery_ml_classification_logistic_regression_create_model1.sql
+++ analytics/bigquery_ml_classification_logistic_regression_create_model1.sql
@@ -23,40 +23,40 @@
 
 CREATE OR REPLACE MODEL `ecommerce.classification_model1`
 OPTIONS (
-  model_type='logistic_reg',
-  labels = ['will_buy_on_return_visit']
+    model_type='logistic_reg',
+    labels = ['will_buy_on_return_visit']
 ) AS
 
 -- training data provided below via SELECT statement
 
 SELECT
-  * EXCEPT(fullVisitorId)
+    * EXCEPT(fullVisitorId)
 FROM
 
-  -- features
-  (
-    SELECT
-      fullVisitorId,
-      IFNULL(totals.bounces, 0) AS bounces,
-      IFNULL(totals.timeOnSite, 0) AS time_on_site
-    FROM
-      `data-to-insights.ecommerce.web_analytics`
-    WHERE
-      totals.newVisits = 1
-      -- train on subset of the data so we can test on the rest of the data
-      AND date BETWEEN '20160801' AND '20170430'
-  )
-  JOIN
-  (
-    SELECT
-      fullvisitorid,
-      -- the label of answers to train on
-      IF(COUNTIF(totals.transactions > 0 AND totals.newVisits IS NULL) > 0, 1, 0) AS will_buy_on_return_visit
-    FROM
-      `data-to-insights.ecommerce.web_analytics`
-    GROUP BY
-      fullvisitorid
-  )
-  USING
-    (fullVisitorId)
+    -- features
+    (
+      SELECT
+        fullVisitorId,
+        IFNULL(totals.bounces, 0) AS bounces,
+        IFNULL(totals.timeOnSite, 0) AS time_on_site
+      FROM
+        `data-to-insights.ecommerce.web_analytics`
+      WHERE
+        totals.newVisits = 1
+        -- train on subset of the data so we can test on the rest of the data
+        AND date BETWEEN '20160801' AND '20170430'
+    )
+    JOIN
+    (
+      SELECT
+        fullvisitorid,
+        -- the label of answers to train on
+        IF(COUNTIF(totals.transactions > 0 AND totals.newVisits IS NULL) > 0, 1, 0) AS will_buy_on_return_visit
+      FROM
+        `data-to-insights.ecommerce.web_analytics`
+      GROUP BY
+        fullvisitorid
+    )
+    USING
+      (fullVisitorId)
 ;
