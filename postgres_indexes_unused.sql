87457e33101e2cb8385852a428c04d3bee58d457
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:46:32 +0300

    updated postgres_indexes_unused.sql

--- postgres_indexes_unused.sql
+++ postgres_indexes_unused.sql
@@ -18,21 +18,21 @@
 -- Tested on PostgreSQL 8.4, 9.x, 10.x, 11.x, 12.x, 13.0
 
 SELECT
-  relname AS table,
-  indexrelname AS index,
-  idx_scan,
-  idx_tup_read,
-  idx_tup_fetch,
-  pg_size_pretty(pg_relation_size(indexrelname::regclass))
+    relname AS table,
+    indexrelname AS index,
+    idx_scan,
+    idx_tup_read,
+    idx_tup_fetch,
+    pg_size_pretty(pg_relation_size(indexrelname::regclass))
 FROM
-  pg_stat_all_indexes
+    pg_stat_all_indexes
 WHERE
-  schemaname = 'public'
-    AND
-  idx_scan = 0
-    AND
-  idx_tup_read = 0
-    AND
-  idx_tup_fetch = 0
+    schemaname = 'public'
+        AND
+    idx_scan = 0
+        AND
+    idx_tup_read = 0
+        AND
+    idx_tup_fetch = 0
 ORDER BY
-  pg_relation_size(indexrelname::regclass) DESC;
+    pg_relation_size(indexrelname::regclass) DESC;
