b4a708760e38847b3449f760855726522fcb5a5c
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:48:49 +0300

    updated postgres_settings_error_handling.sql

--- postgres_settings_error_handling.sql
+++ postgres_settings_error_handling.sql
@@ -24,21 +24,21 @@
 -- https://www.postgresql.org/docs/12/runtime-config-error-handling.html
 
 SELECT
-  name,
-  setting,
-  -- category,
-  vartype,
-  short_desc,
-  -- enumvals,
-  source,
-  min_val,
-  max_val,
-  boot_val,
-  reset_val,
-  -- not available on PostgreSQL < 9.5
-  pending_restart
+    name,
+    setting,
+    -- category,
+    vartype,
+    short_desc,
+    -- enumvals,
+    source,
+    min_val,
+    max_val,
+    boot_val,
+    reset_val,
+    -- not available on PostgreSQL < 9.5
+    pending_restart
 FROM
-  pg_settings
+    pg_settings
 WHERE
-  -- Error Handling
-  category ILIKE '%Error%';
+    -- Error Handling
+    category ILIKE '%Error%';
