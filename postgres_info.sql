f071662e62810d4cb367b48f79a197c40413372e
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:46:50 +0300

    updated postgres_info.sql

--- postgres_info.sql
+++ postgres_info.sql
@@ -32,9 +32,9 @@
 -- version() returns a long human readable string, hence we split from others SELECTs eg.
 -- PostgreSQL 12.3 (Debian 12.3-1.pgdg100+1) on x86_64-pc-linux-gnu, compiled by gcc (Debian 8.3.0-6) 8.3.0, 64-bit
 SELECT
-  version(),
-  current_setting('server_version') AS "server_version",
-  current_setting('server_version_num') AS "server_version_num";
+    version(),
+    current_setting('server_version') AS "server_version",
+    current_setting('server_version_num') AS "server_version_num";
 
 -- ========================================================================== --
 --                          S e r v e r   D e t a i l s
@@ -44,18 +44,18 @@ SELECT
 --\pset title 'PostgreSQL Server Details'
 
 SELECT
-  pg_postmaster_start_time(),
-  pg_conf_load_time(),
-  current_setting('logging_collector') AS "logging_collector",
-  current_setting('log_destination') AS "log_destination",
-  -- not available in Postgres 9
-  -- doesn't work because it still checks if pg_current_logfile() is valid and neither eval or execute seem to work around this
-  -- CASE WHEN current_setting('server_version_num')::int > 100000 THEN pg_current_logfile() ELSE NULL END as pg_current_logfile
-  pg_current_logfile()
-  -- current_setting('log_directory') AS "log_directory",  -- log
-  -- current_setting('log_filename') AS "log_filename",    -- postgresql-%Y-%m-%d_%H%M%S.log
-  -- not available on Postgres 10
-  --pg_jit_available()
+    pg_postmaster_start_time(),
+    pg_conf_load_time(),
+    current_setting('logging_collector') AS "logging_collector",
+    current_setting('log_destination') AS "log_destination",
+    -- not available in Postgres 9
+    -- doesn't work because it still checks if pg_current_logfile() is valid and neither eval or execute seem to work around this
+    -- CASE WHEN current_setting('server_version_num')::int > 100000 THEN pg_current_logfile() ELSE NULL END as pg_current_logfile
+    pg_current_logfile()
+    -- current_setting('log_directory') AS "log_directory",  -- log
+    -- current_setting('log_filename') AS "log_filename",    -- postgresql-%Y-%m-%d_%H%M%S.log
+    -- not available on Postgres 10
+    --pg_jit_available()
 ;
 
 -- SELECT pg_reload_conf(), pg_rotate_logfile();
@@ -87,18 +87,18 @@ SELECT
 --\pset title 'Config Files'
 
 SELECT
-  current_setting('config_file') AS "config_file",
-  current_setting('hba_file') AS "hba_file",
-  current_setting('ident_file') AS "ident_file";
+    current_setting('config_file') AS "config_file",
+    current_setting('hba_file') AS "hba_file",
+    current_setting('ident_file') AS "ident_file";
 
 \echo
 --\pset title 'PostgreSQL Data Directory & Unix Sockets'
 
 SELECT
-  current_setting('data_directory') AS "data_directory",
-  current_setting('unix_socket_directories') AS "unix_socket_directories",
-  current_setting('unix_socket_permissions') AS "unix_socket_permissions",
-  current_setting('unix_socket_group') AS "unix_socket_group";
+    current_setting('data_directory') AS "data_directory",
+    current_setting('unix_socket_directories') AS "unix_socket_directories",
+    current_setting('unix_socket_permissions') AS "unix_socket_permissions",
+    current_setting('unix_socket_group') AS "unix_socket_group";
 
 -- ========================================================================== --
 --                   B u f f e r s   &   C o n n e c t i o n s
@@ -108,13 +108,13 @@ SELECT
 --\pset title 'Buffers & Connections'
 
 SELECT
-  current_setting('shared_buffers') AS "shared_buffers",
-  current_setting('work_mem') AS "work_mem",
-  current_setting('max_connections') AS "max_connections",
-  current_setting('max_files_per_process') AS "max_files_per_process", -- should be less than ulimit nofiles to avoid “Too many open files” failures
-  current_setting('track_activities') AS "track_activities", -- for pg_stat / pg_statio family of system views that are used in many other adjacent scripts
-  current_setting('track_counts') AS "track_counts", -- needed for the autovacuum daemon
-  current_setting('password_encryption') AS "password_encryption";
+    current_setting('shared_buffers') AS "shared_buffers",
+    current_setting('work_mem') AS "work_mem",
+    current_setting('max_connections') AS "max_connections",
+    current_setting('max_files_per_process') AS "max_files_per_process", -- should be less than ulimit nofiles to avoid “Too many open files” failures
+    current_setting('track_activities') AS "track_activities", -- for pg_stat / pg_statio family of system views that are used in many other adjacent scripts
+    current_setting('track_counts') AS "track_counts", -- needed for the autovacuum daemon
+    current_setting('password_encryption') AS "password_encryption";
 
 
 -- ========================================================================== --
@@ -126,12 +126,12 @@ SELECT
 
 -- in SQL the following have special syntax and should be called without parens: current_catalog, current_role, current_schema, current_user, session_user
 SELECT
-  current_user,  -- aka user, current_role - this is the effective user for permission checking
-  session_user,  -- connection user before superuser SET SESSION AUTHORIZATION
-  current_schema,
-  current_catalog, -- SQL standard, same as current_database()
-  pg_backend_pid(),
-  current_query();
+    current_user,  -- aka user, current_role - this is the effective user for permission checking
+    session_user,  -- connection user before superuser SET SESSION AUTHORIZATION
+    current_schema,
+    current_catalog, -- SQL standard, same as current_database()
+    pg_backend_pid(),
+    current_query();
 
 \echo
 --\pset title 'Schema search list'
@@ -147,16 +147,16 @@ SELECT current_schemas(true) AS "current_schemas(true) - auto-searched schemas";
 --\pset title 'Backup & Recovery'
 
 SELECT
-  pg_is_in_backup(),
-  pg_is_in_recovery(),
-  pg_backup_start_time(),
-  'see postgres_recovery.sql for more info' AS "info";
-  -- use postgres_recovery.sql instead of having a big blank table distracting us here
-  -- the following recovery control functions can only be executed during recovery - to get just the above use postgres_funcs.sql
-  --( CASE WHEN pg_is_in_recovery() THEN pg_is_wal_replay_paused() END)       AS "pg_is_wal_replay_paused()",
-  --( CASE WHEN pg_is_in_recovery() THEN pg_last_wal_receive_lsn() END)       AS "pg_last_wal_receive_lsn()",
-  --( CASE WHEN pg_is_in_recovery() THEN pg_last_wal_replay_lsn() END)        AS "pg_last_wal_replay_lsn()",
-  --( CASE WHEN pg_is_in_recovery() THEN pg_last_xact_replay_timestamp() END) AS "pg_last_xact_replay_timestamp()"
+    pg_is_in_backup(),
+    pg_is_in_recovery(),
+    pg_backup_start_time(),
+    'see postgres_recovery.sql for more info' AS "info";
+    -- use postgres_recovery.sql instead of having a big blank table distracting us here
+    -- the following recovery control functions can only be executed during recovery - to get just the above use postgres_funcs.sql
+    --( CASE WHEN pg_is_in_recovery() THEN pg_is_wal_replay_paused() END)       AS "pg_is_wal_replay_paused()",
+    --( CASE WHEN pg_is_in_recovery() THEN pg_last_wal_receive_lsn() END)       AS "pg_last_wal_receive_lsn()",
+    --( CASE WHEN pg_is_in_recovery() THEN pg_last_wal_replay_lsn() END)        AS "pg_last_wal_replay_lsn()",
+    --( CASE WHEN pg_is_in_recovery() THEN pg_last_xact_replay_timestamp() END) AS "pg_last_xact_replay_timestamp()"
 
 --SELECT
 --  pg_ls_logdir(),
@@ -173,10 +173,10 @@ SELECT
 --\pset title 'Networking'
 
 SELECT
-  inet_client_addr(),
-  inet_client_addr(),
-  inet_server_addr(),
-  inet_server_port();
+    inet_client_addr(),
+    inet_client_addr(),
+    inet_server_addr(),
+    inet_server_port();
 
 -- causes 0 rows when mixed with other select funcs
 --SELECT pg_listening_channels();
@@ -193,23 +193,23 @@ SELECT
 --\pset title 'Date & Time'
 
 SELECT
-  -- current timestamps even inside transactions/functions
-  now(),
-  timeofday(),  -- human string timestamp with timezone
+    -- current timestamps even inside transactions/functions
+    now(),
+    timeofday(),  -- human string timestamp with timezone
 
-  -- at start of current transaction for consistency, includes +offset timezone
-  current_timestamp(2) AS "current_timestamp(2)", -- secs precision of 2 decimal places, includes +offset timezone
-  current_date,
-  current_time(1) AS "current_time(1)", -- includes +offset timezone
-  localtime,
-  localtimestamp(1) AS "localtimestamp(1)",
+    -- at start of current transaction for consistency, includes +offset timezone
+    current_timestamp(2) AS "current_timestamp(2)", -- secs precision of 2 decimal places, includes +offset timezone
+    current_date,
+    current_time(1) AS "current_time(1)", -- includes +offset timezone
+    localtime,
+    localtimestamp(1) AS "localtimestamp(1)",
 
-  -- provide current timestamps even inside transactions/functions
-  -- now()
-  -- timeofday(),  -- human string timestamp with timezone
-  clock_timestamp(), -- current date + time (changes throughout function)
-  statement_timestamp(),
-  transaction_timestamp()  -- same as CURRENT_TIMESTAMP
+    -- provide current timestamps even inside transactions/functions
+    -- now()
+    -- timeofday(),  -- human string timestamp with timezone
+    clock_timestamp(), -- current date + time (changes throughout function)
+    statement_timestamp(),
+    transaction_timestamp()  -- same as CURRENT_TIMESTAMP
 ;
 
 --\pset title
