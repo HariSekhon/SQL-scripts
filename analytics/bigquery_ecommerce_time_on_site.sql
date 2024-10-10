b9debed15cdf2cb83e4bd04ca0fc4ee7ad309738
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:52:02 +0300

    updated bigquery_ecommerce_time_on_site.sql

--- analytics/bigquery_ecommerce_time_on_site.sql
+++ analytics/bigquery_ecommerce_time_on_site.sql
@@ -16,32 +16,32 @@
 -- looking at bounces and time on site are not good features for model training and prediction since of the top 10 customers by time on site only 1 purchased!
 
 SELECT
-  * EXCEPT(fullVisitorId)
+    * EXCEPT(fullVisitorId)
 FROM
 
-  # features
-  (SELECT
-    fullVisitorId,
-    IFNULL(totals.bounces, 0) AS bounces,
-    IFNULL(totals.timeOnSite, 0) AS time_on_site
-  FROM
-    `data-to-insights.ecommerce.web_analytics`
-  WHERE
-    totals.newVisits = 1)
-  JOIN
-  (SELECT
-    fullvisitorid,
-    IF(
-      COUNTIF(
-        totals.transactions > 0
-          AND
-        totals.newVisits IS NULL
-      ) > 0, 1, 0
-    ) AS will_buy_on_return_visit
-  FROM
-      `data-to-insights.ecommerce.web_analytics`
-  GROUP BY fullvisitorid)
-  USING (fullVisitorId)
+    # features
+    (SELECT
+        fullVisitorId,
+        IFNULL(totals.bounces, 0) AS bounces,
+        IFNULL(totals.timeOnSite, 0) AS time_on_site
+    FROM
+        `data-to-insights.ecommerce.web_analytics`
+    WHERE
+        totals.newVisits = 1)
+    JOIN
+    (SELECT
+        fullvisitorid,
+        IF(
+            COUNTIF(
+                totals.transactions > 0
+                    AND
+                totals.newVisits IS NULL
+            ) > 0, 1, 0
+        ) AS will_buy_on_return_visit
+    FROM
+        `data-to-insights.ecommerce.web_analytics`
+    GROUP BY fullvisitorid)
+    USING (fullVisitorId)
 ORDER BY
-  time_on_site DESC
+    time_on_site DESC
 LIMIT 10;
