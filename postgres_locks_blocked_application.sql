a0f6c4dd785875408575c6c373a89a8543f766b2
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:47:43 +0300

    updated postgres_locks_blocked_application.sql

--- postgres_locks_blocked_application.sql
+++ postgres_locks_blocked_application.sql
@@ -22,37 +22,37 @@
 -- https://wiki.postgresql.org/wiki/Lock_Monitoring
 
 SELECT
-  blocked_locks.pid                   AS blocked_pid,
-  blocked_activity.usename            AS blocked_user,
-  blocking_locks.pid                  AS blocking_pid,
-  blocking_activity.usename           AS blocking_user,
-  blocked_activity.query              AS blocked_statement,
-  blocking_activity.query             AS current_statement_in_blocking_process,
-  blocked_activity.application_name   AS blocked_application,
-  blocking_activity.application_name  AS blocking_application
+    blocked_locks.pid                   AS blocked_pid,
+    blocked_activity.usename            AS blocked_user,
+    blocking_locks.pid                  AS blocking_pid,
+    blocking_activity.usename           AS blocking_user,
+    blocked_activity.query              AS blocked_statement,
+    blocking_activity.query             AS current_statement_in_blocking_process,
+    blocked_activity.application_name   AS blocked_application,
+    blocking_activity.application_name  AS blocking_application
 FROM
-  pg_catalog.pg_locks           blocked_locks
+    pg_catalog.pg_locks           blocked_locks
 JOIN
-  pg_catalog.pg_stat_activity   blocked_activity
+    pg_catalog.pg_stat_activity   blocked_activity
 ON
-  blocked_activity.pid = blocked_locks.pid
+    blocked_activity.pid = blocked_locks.pid
 JOIN
-  pg_catalog.pg_locks           blocking_locks
+    pg_catalog.pg_locks           blocking_locks
 ON
-  blocking_locks.locktype = blocked_locks.locktype
-  AND blocking_locks.DATABASE       IS NOT DISTINCT FROM blocked_locks.DATABASE
-  AND blocking_locks.relation       IS NOT DISTINCT FROM blocked_locks.relation
-  AND blocking_locks.page           IS NOT DISTINCT FROM blocked_locks.page
-  AND blocking_locks.tuple          IS NOT DISTINCT FROM blocked_locks.tuple
-  AND blocking_locks.virtualxid     IS NOT DISTINCT FROM blocked_locks.virtualxid
-  AND blocking_locks.transactionid  IS NOT DISTINCT FROM blocked_locks.transactionid
-  AND blocking_locks.classid        IS NOT DISTINCT FROM blocked_locks.classid
-  AND blocking_locks.objid          IS NOT DISTINCT FROM blocked_locks.objid
-  AND blocking_locks.objsubid       IS NOT DISTINCT FROM blocked_locks.objsubid
-  AND blocking_locks.pid != blocked_locks.pid
+    blocking_locks.locktype = blocked_locks.locktype
+    AND blocking_locks.DATABASE       IS NOT DISTINCT FROM blocked_locks.DATABASE
+    AND blocking_locks.relation       IS NOT DISTINCT FROM blocked_locks.relation
+    AND blocking_locks.page           IS NOT DISTINCT FROM blocked_locks.page
+    AND blocking_locks.tuple          IS NOT DISTINCT FROM blocked_locks.tuple
+    AND blocking_locks.virtualxid     IS NOT DISTINCT FROM blocked_locks.virtualxid
+    AND blocking_locks.transactionid  IS NOT DISTINCT FROM blocked_locks.transactionid
+    AND blocking_locks.classid        IS NOT DISTINCT FROM blocked_locks.classid
+    AND blocking_locks.objid          IS NOT DISTINCT FROM blocked_locks.objid
+    AND blocking_locks.objsubid       IS NOT DISTINCT FROM blocked_locks.objsubid
+    AND blocking_locks.pid != blocked_locks.pid
 JOIN
-  pg_catalog.pg_stat_activity blocking_activity
+    pg_catalog.pg_stat_activity blocking_activity
 ON
-  blocking_activity.pid = blocking_locks.pid
+    blocking_activity.pid = blocking_locks.pid
 WHERE
-  NOT blocked_locks.granted;
+    NOT blocked_locks.granted;
