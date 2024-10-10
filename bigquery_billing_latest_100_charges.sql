1e6f6e0d9ff4b11edf342fc7df2ff3bb982b809d
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:39:19 +0300

    updated bigquery_billing_latest_100_charges.sql

--- bigquery_billing_latest_100_charges.sql
+++ bigquery_billing_latest_100_charges.sql
@@ -18,22 +18,22 @@
 -- latest 100 charges
 
 SELECT
-  product,
-  resource_type,
-  start_time,
-  end_time,
-  cost,
-  project_id,
-  project_name,
-  project_labels_key,
-  currency,
-  currency_conversion_rate,
-  usage_amount,
-  usage_unit
+    product,
+    resource_type,
+    start_time,
+    end_time,
+    cost,
+    project_id,
+    project_name,
+    project_labels_key,
+    currency,
+    currency_conversion_rate,
+    usage_amount,
+    usage_unit
 FROM
-  `myproject.mydataset.imported_billing_data`
+    `myproject.mydataset.imported_billing_data`
 WHERE
-  cost > 0
+    cost > 0
 ORDER BY
-  end_time DESC
+    end_time DESC
 LIMIT 100;
