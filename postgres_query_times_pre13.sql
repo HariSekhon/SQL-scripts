025f18be6780bc4d897528973f8886c3e62b5c18
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:48:08 +0300

    updated postgres_query_times_pre13.sql

--- postgres_query_times_pre13.sql
+++ postgres_query_times_pre13.sql
@@ -25,20 +25,20 @@
 CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
 
 SELECT
-  calls,
-  rows,
-  ROUND((total_time::numeric / 1000), 4) AS total_secs,
-  -- newer versions of PostgreSQL have mean_time field, don't need to calculate
-  --ROUND((total_time / 1000 / calls)::numeric, 4) AS average_secs,
-  ROUND(mean_time::numeric / 1000, 4) AS average_secs,
-  ROUND(min_time::numeric / 1000, 4) AS min_secs,
-  ROUND(max_time::numeric / 1000, 4) AS max_secs,
-  ROUND(stddev_time::numeric / 1000, 4) AS stddev_secs,
-  query
+    calls,
+    rows,
+    ROUND((total_time::numeric / 1000), 4) AS total_secs,
+    -- newer versions of PostgreSQL have mean_time field, don't need to calculate
+    --ROUND((total_time / 1000 / calls)::numeric, 4) AS average_secs,
+    ROUND(mean_time::numeric / 1000, 4) AS average_secs,
+    ROUND(min_time::numeric / 1000, 4) AS min_secs,
+    ROUND(max_time::numeric / 1000, 4) AS max_secs,
+    ROUND(stddev_time::numeric / 1000, 4) AS stddev_secs,
+    query
 FROM
-  pg_stat_statements
+    pg_stat_statements
 ORDER BY
-  average_secs DESC,
-  calls DESC,
-  rows DESC
+    average_secs DESC,
+    calls DESC,
+    rows DESC
 LIMIT 100;
