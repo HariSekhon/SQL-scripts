dc0547ed98353dd49c757072eeb4195c634fd140
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:42:17 +0300

    updated mysql_views.sql

--- mysql_views.sql
+++ mysql_views.sql
@@ -18,14 +18,14 @@
 -- Tested on MySQL 5.5, 5.6, 5.7, 8.0 and MariaDB 5.5, 10.0 - 10.5
 
 SELECT
-  -- table_catalog,
-  table_schema,
-  table_name,
-  IS_UPDATABLE,
-  DEFINER,
-  SECURITY_TYPE
+    -- table_catalog,
+    table_schema,
+    table_name,
+    IS_UPDATABLE,
+    DEFINER,
+    SECURITY_TYPE
 FROM
-  information_schema.views
+    information_schema.views
 ORDER BY
-  table_schema,
-  table_name;
+    table_schema,
+    table_name;
