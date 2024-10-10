db56705a96ee6bde9325489008ed2a4f4ae858c8
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:48:06 +0300

    updated postgres_query_times.sql

--- postgres_query_times.sql
+++ postgres_query_times.sql
@@ -28,20 +28,20 @@
 CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
 
 SELECT
-  calls,
-  rows,
-  ROUND((total_exec_time::numeric / 1000), 4) AS total_secs,
-  -- newer versions of PostgreSQL have mean_exec_time field, don't need to calculate
-  --ROUND((total_exec_time / 1000 / calls)::numeric, 4) AS average_secs,
-  ROUND(mean_exec_time::numeric / 1000, 4) AS average_secs,
-  ROUND(min_exec_time::numeric / 1000, 4) AS min_secs,
-  ROUND(max_exec_time::numeric / 1000, 4) AS max_secs,
-  ROUND(stddev_exec_time::numeric / 1000, 4) AS stddev_secs,
-  query
+    calls,
+    rows,
+    ROUND((total_exec_time::numeric / 1000), 4) AS total_secs,
+    -- newer versions of PostgreSQL have mean_exec_time field, don't need to calculate
+    --ROUND((total_exec_time / 1000 / calls)::numeric, 4) AS average_secs,
+    ROUND(mean_exec_time::numeric / 1000, 4) AS average_secs,
+    ROUND(min_exec_time::numeric / 1000, 4) AS min_secs,
+    ROUND(max_exec_time::numeric / 1000, 4) AS max_secs,
+    ROUND(stddev_exec_time::numeric / 1000, 4) AS stddev_secs,
+    query
 FROM
-  pg_stat_statements
+    pg_stat_statements
 ORDER BY
-  average_secs DESC,
-  calls DESC,
-  rows DESC
+    average_secs DESC,
+    calls DESC,
+    rows DESC
 LIMIT 100;
