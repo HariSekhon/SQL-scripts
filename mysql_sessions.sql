e3b08d3ad07bf90ce80b5f235c58db6c0042c83d
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:41:41 +0300

    updated mysql_sessions.sql

--- mysql_sessions.sql
+++ mysql_sessions.sql
@@ -23,31 +23,31 @@
 -- sys doesn't exist in MySQL <= 5.6 or MariaDB 10.5
 
 SELECT
-  user,
-  db,
-  program_name,
-  command,
-  state,
-  time,
-  current_statement,
-  current_memory,
-  progress,
-  lock_latency,
-  rows_examined,
-  rows_sent,
-  rows_affected,
-  tmp_tables,
-  tmp_disk_tables,
-  full_scan,
-  last_statement,
-  last_statement_latency,
-  last_wait,
-  last_wait_latency,
-  trx_latency,
-  trx_state
+    user,
+    db,
+    program_name,
+    command,
+    state,
+    time,
+    current_statement,
+    current_memory,
+    progress,
+    lock_latency,
+    rows_examined,
+    rows_sent,
+    rows_affected,
+    tmp_tables,
+    tmp_disk_tables,
+    full_scan,
+    last_statement,
+    last_statement_latency,
+    last_wait,
+    last_wait_latency,
+    trx_latency,
+    trx_state
 FROM
-  sys.session
+    sys.session
 WHERE
-  user <> 'sql/event_scheduler'
+    user <> 'sql/event_scheduler'
 ORDER BY
-  current_memory DESC;
+    current_memory DESC;
