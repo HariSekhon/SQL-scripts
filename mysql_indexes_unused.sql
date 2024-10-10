f4a17ae78afe68674e99fd2c99a27c561bffc57b
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:41:23 +0300

    updated mysql_indexes_unused.sql

--- mysql_indexes_unused.sql
+++ mysql_indexes_unused.sql
@@ -35,10 +35,10 @@ SELECT IF (@@performance_schema, 'TRUE', 'FALSE') AS 'Performance Schema enabled
 \! echo "Unused Indexes since startup:";
 
 SELECT
-  *
+    *
 FROM
-  sys.schema_unused_indexes
+    sys.schema_unused_indexes
 ORDER BY
-  object_schema,
-  object_name,
-  index_name;
+    object_schema,
+    object_name,
+    index_name;
