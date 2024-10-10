6e8caf3da55cf24e77c71ada550777775a673d68
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:51:02 +0300

    updated bigquery_ecommerce_conversion_rate.sql

--- analytics/bigquery_ecommerce_conversion_rate.sql
+++ analytics/bigquery_ecommerce_conversion_rate.sql
@@ -14,24 +14,24 @@
 --
 
 WITH visitors AS(
-  SELECT
-  COUNT(DISTINCT fullVisitorId) AS total_visitors
-  FROM `data-to-insights.ecommerce.web_analytics`
+    SELECT
+    COUNT(DISTINCT fullVisitorId) AS total_visitors
+    FROM `data-to-insights.ecommerce.web_analytics`
 ),
 
 purchasers AS(
-  SELECT
-  COUNT(DISTINCT fullVisitorId) AS total_purchasers
-  FROM `data-to-insights.ecommerce.web_analytics`
-  WHERE totals.transactions IS NOT NULL
+    SELECT
+    COUNT(DISTINCT fullVisitorId) AS total_purchasers
+    FROM `data-to-insights.ecommerce.web_analytics`
+    WHERE totals.transactions IS NOT NULL
 )
 
 SELECT
-  total_visitors,
-  total_purchasers,
-  total_purchasers / total_visitors AS conversion_rate,
-  ROUND(total_purchasers / total_visitors * 100, 2) AS conversion_percentage
+    total_visitors,
+    total_purchasers,
+    total_purchasers / total_visitors AS conversion_rate,
+    ROUND(total_purchasers / total_visitors * 100, 2) AS conversion_percentage
 FROM
-  visitors,
-  purchasers
+    visitors,
+    purchasers
 ;
