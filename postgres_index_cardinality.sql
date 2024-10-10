752ba82dad30fee9b15f7ae8fbc40d6f4c2978b6
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:46:00 +0300

    updated postgres_index_cardinality.sql

--- postgres_index_cardinality.sql
+++ postgres_index_cardinality.sql
@@ -18,17 +18,17 @@
 -- Tested on PostgreSQL 8.4, 9.x, 10.x, 11.x, 12.x, 13.0
 
 SELECT
-  relname,
-  --relkind,
-  reltuples AS "cardinality (reltuples)",
-  relpages
+    relname,
+    --relkind,
+    reltuples AS "cardinality (reltuples)",
+    relpages
 FROM
-  pg_class
+    pg_class
 WHERE
-  relkind = 'i'
---    AND
---  relname LIKE 'someprefix%';
-    AND
-  relname NOT ILIKE 'pg_%'
+    relkind = 'i'
+    --    AND
+    --relname LIKE 'someprefix%';
+        AND
+    relname NOT ILIKE 'pg_%'
 ORDER BY
-  2 DESC;
+    2 DESC;
