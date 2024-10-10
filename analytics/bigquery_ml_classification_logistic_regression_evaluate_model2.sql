1c66587296cf334dc965ca93d661c15a109a80f4
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:57:47 +0300

    updated bigquery_ml_classification_logistic_regression_evaluate_model2.sql

--- analytics/bigquery_ml_classification_logistic_regression_evaluate_model2.sql
+++ analytics/bigquery_ml_classification_logistic_regression_evaluate_model2.sql
@@ -20,73 +20,77 @@
 -- good ROC auc > 0.9 - gives much better predictive power than model1
 
 SELECT
-  roc_auc,
-  CASE
-    WHEN roc_auc > .9 THEN 'good'
-    WHEN roc_auc > .8 THEN 'fair'
-    WHEN roc_auc > .7 THEN 'not great'
-  ELSE 'poor' END AS model_quality,
-  accuracy,
-  precision,
-  recall
+    roc_auc,
+    CASE
+        WHEN roc_auc > .9 THEN 'good'
+        WHEN roc_auc > .8 THEN 'fair'
+        WHEN roc_auc > .7 THEN 'not great'
+    ELSE 'poor' END AS model_quality,
+    accuracy,
+    precision,
+    recall
 FROM
-  ML.EVALUATE(MODEL ecommerce.classification_model_2,  (
+    ML.EVALUATE(MODEL ecommerce.classification_model_2,  (
 
-WITH all_visitor_stats AS (
-SELECT
-  fullvisitorid,
-  IF(COUNTIF(totals.transactions > 0 AND totals.newVisits IS NULL) > 0, 1, 0) AS will_buy_on_return_visit
-  FROM `data-to-insights.ecommerce.web_analytics`
-  GROUP BY fullvisitorid
-)
+        WITH all_visitor_stats AS (
+        SELECT
+            fullvisitorid,
+            IF(COUNTIF(totals.transactions > 0 AND totals.newVisits IS NULL) > 0, 1, 0) AS will_buy_on_return_visit
+            FROM `data-to-insights.ecommerce.web_analytics`
+            GROUP BY fullvisitorid
+        )
 
 # add in new features
 SELECT * EXCEPT(unique_session_id) FROM (
 
-  SELECT
-      CONCAT(fullvisitorid, CAST(visitId AS STRING)) AS unique_session_id,
+      SELECT
+          CONCAT(fullvisitorid, CAST(visitId AS STRING)) AS unique_session_id,
 
-      # labels
-      will_buy_on_return_visit,
+          # labels
+          will_buy_on_return_visit,
 
-      MAX(CAST(h.eCommerceAction.action_type AS INT64)) AS latest_ecommerce_progress,
+          MAX(CAST(h.eCommerceAction.action_type AS INT64)) AS latest_ecommerce_progress,
 
-      # behavior on the site
-      IFNULL(totals.bounces, 0) AS bounces,
-      IFNULL(totals.timeOnSite, 0) AS time_on_site,
-      totals.pageviews,
+          # behavior on the site
+          IFNULL(totals.bounces, 0) AS bounces,
+          IFNULL(totals.timeOnSite, 0) AS time_on_site,
+          totals.pageviews,
 
-      # where the visitor came from
-      trafficSource.source,
-      trafficSource.medium,
-      channelGrouping,
+          # where the visitor came from
+          trafficSource.source,
+          trafficSource.medium,
+          channelGrouping,
 
-      # mobile or desktop
-      device.deviceCategory,
+          # mobile or desktop
+          device.deviceCategory,
 
-      # geographic
-      IFNULL(geoNetwork.country, "") AS country
+          # geographic
+          IFNULL(geoNetwork.country, "") AS country
 
-  FROM `data-to-insights.ecommerce.web_analytics`,
-     UNNEST(hits) AS h
+      FROM `data-to-insights.ecommerce.web_analytics`,
+          UNNEST(hits) AS h
 
-    JOIN all_visitor_stats USING(fullvisitorid)
+      JOIN
+          all_visitor_stats
 
-  WHERE 1=1
-    # only predict for new visits
-    AND totals.newVisits = 1
-    AND date BETWEEN '20170501' AND '20170630' # eval 2 months
+      USING(fullvisitorid)
 
-  GROUP BY
-  unique_session_id,
-  will_buy_on_return_visit,
-  bounces,
-  time_on_site,
-  totals.pageviews,
-  trafficSource.source,
-  trafficSource.medium,
-  channelGrouping,
-  device.deviceCategory,
-  country
-)
-));
+      WHERE 1=1
+          # only predict for new visits
+          AND totals.newVisits = 1
+          AND date BETWEEN '20170501' AND '20170630' # eval 2 months
+
+      GROUP BY
+          unique_session_id,
+          will_buy_on_return_visit,
+          bounces,
+          time_on_site,
+          totals.pageviews,
+          trafficSource.source,
+          trafficSource.medium,
+          channelGrouping,
+          device.deviceCategory,
+          country
+        )
+    )
+);
