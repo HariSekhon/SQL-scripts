e1ddebde1bd5ed16ac8a12e8bb86a494725913c9
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:46:18 +0300

    updated postgres_index_cardinality_with_schema_name.sql

--- postgres_index_cardinality_with_schema_name.sql
+++ postgres_index_cardinality_with_schema_name.sql
@@ -18,32 +18,32 @@
 -- Tested on PostgreSQL 8.4, 9.x, 10.x 11.x, 12.x, 13.0
 
 SELECT
-  schema_name,
-  relname,
-  reltuples AS "cardinality (reltuples)",
-  relpages
-FROM (
-  SELECT
-    pg_catalog.pg_namespace.nspname AS schema_name,
+    schema_name,
     relname,
-    reltuples,
+    reltuples AS "cardinality (reltuples)",
     relpages
-  FROM
-    pg_class
-  JOIN
-    pg_namespace ON relnamespace = pg_namespace.oid
-  WHERE
-    relkind = 'i'
+FROM (
+    SELECT
+        pg_catalog.pg_namespace.nspname AS schema_name,
+        relname,
+        reltuples,
+        relpages
+    FROM
+        pg_class
+    JOIN
+        pg_namespace ON relnamespace = pg_namespace.oid
+    WHERE
+        relkind = 'i'
 ) t
 WHERE
-  schema_name NOT ILIKE 'pg_%'
-    AND
-  schema_name <> 'information_schema'
+    schema_name NOT ILIKE 'pg_%'
+      AND
+    schema_name <> 'information_schema'
 --    AND
 --  relname LIKE 'someprefix%';
-    AND
-  relname NOT ILIKE 'pg_%'
+      AND
+    relname NOT ILIKE 'pg_%'
 ORDER BY
-  3 DESC,
-  schema_name,
-  relname;
+    3 DESC,
+    schema_name,
+    relname;
