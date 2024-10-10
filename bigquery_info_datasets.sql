b1b3f67b0742f7aa1606ca3ed8af751a74e95fc3
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:40:19 +0300

    updated bigquery_info_datasets.sql

--- bigquery_info_datasets.sql
+++ bigquery_info_datasets.sql
@@ -20,17 +20,17 @@
 -- Processes 20MB
 
 SELECT
-  s.*,
-  TIMESTAMP_DIFF(CURRENT_TIMESTAMP(), creation_time, DAY) AS days_live,
-  option_value AS dataset_description
+    s.*,
+    TIMESTAMP_DIFF(CURRENT_TIMESTAMP(), creation_time, DAY) AS days_live,
+    option_value AS dataset_description
 FROM
-  -- XXX: can prefix INFORMATION_SCHEMA with your '<project_id>.' to select from another project, but defaulting to the current project is more convenient than editing this
-  `INFORMATION_SCHEMA.SCHEMATA` AS s
-  LEFT JOIN `INFORMATION_SCHEMA.SCHEMATA_OPTIONS` AS so
-  USING (schema_name)
+    -- XXX: can prefix INFORMATION_SCHEMA with your '<project_id>.' to select from another project, but defaulting to the current project is more convenient than editing this
+    `INFORMATION_SCHEMA.SCHEMATA` AS s
+    LEFT JOIN `INFORMATION_SCHEMA.SCHEMATA_OPTIONS` AS so
+    USING (schema_name)
 -- will miss all the datasets without descriptions like this
 --WHERE
---  so.option_name = 'description'
+--    so.option_name = 'description'
 ORDER BY
-  last_modified_time DESC
+    last_modified_time DESC
 LIMIT 15;
