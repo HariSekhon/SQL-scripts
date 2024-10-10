1befa15a0ef4e3a15afa9b72104a227646671367
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:47:26 +0300

    updated postgres_last_vacuum_analyze_pre94.sql

--- postgres_last_vacuum_analyze_pre94.sql
+++ postgres_last_vacuum_analyze_pre94.sql
@@ -18,28 +18,28 @@
 -- Tested on PostgreSQL 8.4, 9.x, 10.x, 11.x, 12.x, 13.0
 
 SELECT
-  schemaname,
-  relname,
-  n_live_tup,
-  n_dead_tup,
-  n_dead_tup / GREATEST(n_live_tup + n_dead_tup, 1)::float * 100 AS dead_percentage,
-  -- not available on PostgreSQL <= 9.3
-  --n_mod_since_analyze,
-  last_vacuum,
-  last_autovacuum,
-  last_analyze,
-  last_autoanalyze
-  -- not available on PostgreSQL <= 9.0
-  --vacuum_count,
-  --autovacuum_count,
-  -- not available on PostgreSQL <= 9.0
-  --analyze_count,
-  --autoanalyze_count
+    schemaname,
+    relname,
+    n_live_tup,
+    n_dead_tup,
+    n_dead_tup / GREATEST(n_live_tup + n_dead_tup, 1)::float * 100 AS dead_percentage,
+    -- not available on PostgreSQL <= 9.3
+    --n_mod_since_analyze,
+    last_vacuum,
+    last_autovacuum,
+    last_analyze,
+    last_autoanalyze
+    -- not available on PostgreSQL <= 9.0
+    --vacuum_count,
+    --autovacuum_count,
+    -- not available on PostgreSQL <= 9.0
+    --analyze_count,
+    --autoanalyze_count
 FROM
-  pg_stat_user_tables
+    pg_stat_user_tables
 ORDER BY
-  n_dead_tup DESC,
-  last_vacuum DESC,
-  last_analyze DESC,
-  last_autovacuum DESC,
-  last_autoanalyze DESC;
+    n_dead_tup DESC,
+    last_vacuum DESC,
+    last_analyze DESC,
+    last_autovacuum DESC,
+    last_autoanalyze DESC;
