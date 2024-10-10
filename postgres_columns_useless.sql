b41758ca5bf62a1cce8e2a3bc18820cf443d115d
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:44:20 +0300

    updated postgres_columns_useless.sql

--- postgres_columns_useless.sql
+++ postgres_columns_useless.sql
@@ -23,56 +23,56 @@
 
 
 SELECT
-  nspname,
-  relname,
-  attname,
-  typname,
-  (stanullfrac*100)::int AS null_percent,
-  case
-    when stadistinct >= 0
-      then stadistinct
-    else
-      abs(stadistinct)*reltuples
-  end AS "distinct",
-  case 1
-    when stakind1
-      then stavalues1
-    when stakind2
-      then stavalues2
-  end AS "values"
+    nspname,
+    relname,
+    attname,
+    typname,
+    (stanullfrac*100)::int AS null_percent,
+    case
+        when stadistinct >= 0
+            then stadistinct
+        else
+            abs(stadistinct)*reltuples
+    end AS "distinct",
+    case 1
+        when stakind1
+            then stavalues1
+        when stakind2
+            then stavalues2
+    end AS "values"
 FROM
-  pg_class c
+    pg_class c
 JOIN
-  pg_namespace ns
+    pg_namespace ns
 ON
-  (ns.oid=relnamespace)
+    (ns.oid=relnamespace)
 JOIN
-  pg_attribute
+    pg_attribute
 ON
-  (c.oid=attrelid)
+    (c.oid=attrelid)
 JOIN
-  pg_type t
+    pg_type t
 ON
-  (t.oid=atttypid)
+    (t.oid=atttypid)
 JOIN
-  pg_statistic
+    pg_statistic
 ON
-  (c.oid=starelid AND staattnum=attnum)
+    (c.oid=starelid AND staattnum=attnum)
 WHERE
-  nspname NOT LIKE E'pg\\_%'
-    AND
-  nspname != 'information_schema'
-    AND
-  relkind = 'r'
-    AND
-  NOT attisdropped
-    AND
-  attstattarget != 0
-    AND
-  reltuples >= 100
-    AND
-  stadistinct BETWEEN 0 AND 1
+    nspname NOT LIKE E'pg\\_%'
+        AND
+    nspname != 'information_schema'
+        AND
+    relkind = 'r'
+        AND
+    NOT attisdropped
+        AND
+    attstattarget != 0
+        AND
+    reltuples >= 100
+        AND
+    stadistinct BETWEEN 0 AND 1
 ORDER BY
-  nspname,
-  relname,
-  attname;
+    nspname,
+    relname,
+    attname;
