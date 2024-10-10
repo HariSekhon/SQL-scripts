1f36eda42e070c9f702215f053d96ed51ade6ef8
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:45:44 +0300

    updated postgres_idle_sessions_pre92.sql

--- postgres_idle_sessions_pre92.sql
+++ postgres_idle_sessions_pre92.sql
@@ -24,30 +24,30 @@
 -- Tested on PostgreSQL 8.4, 9.0, 9.1
 
 SELECT
-  rank() over (partition by client_addr order by backend_start ASC) as rank,
-  procpid,
-  backend_start,
-  query_start,
-  waiting,
-  datname,
-  usename,
-  client_addr
+    rank() over (partition by client_addr order by backend_start ASC) as rank,
+    procpid,
+    backend_start,
+    query_start,
+    waiting,
+    datname,
+    usename,
+    client_addr
 FROM
-  pg_stat_activity
+    pg_stat_activity
 WHERE
-  -- don't kill yourself
-  procpid <> pg_backend_pid()
-  --  AND
-  -- don't kill your admin tools
-  --application_name !~ '(?:psql)|(?:pgAdmin.+)'
-  --  AND
-  --usename not in ('postgres')
-    AND
-  current_query in ('')
-    AND
-  (
-    (current_timestamp - query_start) > interval '10 minutes'
-      OR
-    (query_start IS NULL AND (current_timestamp - backend_start) > interval '10 minutes')
-  )
+    -- don't kill yourself
+    procpid <> pg_backend_pid()
+    --  AND
+    -- don't kill your admin tools
+    --application_name !~ '(?:psql)|(?:pgAdmin.+)'
+    --  AND
+    --usename not in ('postgres')
+        AND
+    current_query in ('')
+        AND
+    (
+      (current_timestamp - query_start) > interval '10 minutes'
+          OR
+      (query_start IS NULL AND (current_timestamp - backend_start) > interval '10 minutes')
+    )
 ;
