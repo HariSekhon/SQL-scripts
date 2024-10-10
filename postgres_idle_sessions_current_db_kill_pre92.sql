4b0722cb46f19f4e428acfc4be17c006d5131552
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:45:26 +0300

    updated postgres_idle_sessions_current_db_kill_pre92.sql

--- postgres_idle_sessions_current_db_kill_pre92.sql
+++ postgres_idle_sessions_current_db_kill_pre92.sql
@@ -20,29 +20,29 @@
 -- Tested on PostgreSQL 8.4, 9.0, 9.1
 
 SELECT
-  pg_terminate_backend(procpid)
+    pg_terminate_backend(procpid)
 FROM
-  pg_stat_activity
+    pg_stat_activity
 WHERE
-  -- don't kill yourself
-  procpid <> pg_backend_pid()
-  --  AND
-  -- don't kill your admin tools
-  --application_name !~ '(?:psql)|(?:pgAdmin.+)'
-  --  AND
-  --usename not in ('postgres')
-    AND
-  datname = current_database()
-    AND
-  current_query in ('')
-    AND
-  waiting
-  --state in ('idle', 'idle in transaction', 'idle in transaction (aborted)', 'disabled')
-    AND
-  --state_change < current_timestamp - INTERVAL '15' MINUTE;
-  (
-    (current_timestamp - query_start) > interval '15 minutes'
-      OR
-    (query_start IS NULL AND (current_timestamp - backend_start) > interval '15 minutes')
-  )
+    -- don't kill yourself
+    procpid <> pg_backend_pid()
+    --  AND
+    -- don't kill your admin tools
+    --application_name !~ '(?:psql)|(?:pgAdmin.+)'
+    --  AND
+    --usename not in ('postgres')
+        AND
+    datname = current_database()
+        AND
+    current_query in ('')
+        AND
+    waiting
+    --state in ('idle', 'idle in transaction', 'idle in transaction (aborted)', 'disabled')
+        AND
+    --state_change < current_timestamp - INTERVAL '15' MINUTE;
+    (
+      (current_timestamp - query_start) > interval '15 minutes'
+          OR
+      (query_start IS NULL AND (current_timestamp - backend_start) > interval '15 minutes')
+    )
 ;
