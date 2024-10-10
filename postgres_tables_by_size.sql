7e300383bb6309e4976fbf18183e9f28cba321cd
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:50:34 +0300

    updated postgres_tables_by_size.sql

--- postgres_tables_by_size.sql
+++ postgres_tables_by_size.sql
@@ -18,19 +18,19 @@
 -- Tested on PostgreSQL 8.4, 9.x, 10.x, 11.x, 12.x, 13.0
 
 SELECT
-  --nspname || '.' || relname AS relation,
-  nspname,
-  relname,
-  pg_size_pretty(pg_total_relation_size(C.oid)) AS total_size
+    --nspname || '.' || relname AS relation,
+    nspname,
+    relname,
+    pg_size_pretty(pg_total_relation_size(C.oid)) AS total_size
 FROM
-  pg_class C
+    pg_class C
 LEFT JOIN
-  pg_namespace N ON (N.oid = C.relnamespace)
+    pg_namespace N ON (N.oid = C.relnamespace)
 WHERE
-  nspname NOT IN ('pg_catalog', 'information_schema')
-    AND
-  C.relkind <> 'i'
-    AND
-  nspname !~ '^pg_toast'
+    nspname NOT IN ('pg_catalog', 'information_schema')
+        AND
+    C.relkind <> 'i'
+        AND
+    nspname !~ '^pg_toast'
 ORDER BY
-  pg_total_relation_size(C.oid) DESC;
+    pg_total_relation_size(C.oid) DESC;
