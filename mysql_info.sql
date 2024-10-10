151b494653945f9547688d61395734188bb66e79
Hari Sekhon <harisekhon@gmail.com>
2024-10-10 14:41:32 +0300

    updated mysql_info.sql

--- mysql_info.sql
+++ mysql_info.sql
@@ -25,18 +25,18 @@
 
 -- some of these allow not using the brackets but use them for portability between MySQL 5.x and 8.x
 SELECT
-  VERSION(),
-  USER(),         -- client's reported user + host, same as SESSION_USER(), SYSTEM_USER()
-  CURRENT_USER(), -- authenticated user name + host name - this is the one you want for debugging your mysql.user table configuration
-  DATABASE(),     -- SCHEMA()
-  NOW(),
-  CURDATE(),
-  CURTIME(),
-  UTC_DATE(),
-  UTC_TIME(),
-  UTC_TIMESTAMP(),
-  SYSDATE(),      -- returns date of function completion
-  -- PS_CURRENT_THREAD_ID(),  -- MySQL 8.0.16+
-  UUID_SHORT(),   -- integer
-  UUID()          -- alnum
+    VERSION(),
+    USER(),         -- client's reported user + host, same as SESSION_USER(), SYSTEM_USER()
+    CURRENT_USER(), -- authenticated user name + host name - this is the one you want for debugging your mysql.user table configuration
+    DATABASE(),     -- SCHEMA()
+    NOW(),
+    CURDATE(),
+    CURTIME(),
+    UTC_DATE(),
+    UTC_TIME(),
+    UTC_TIMESTAMP(),
+    SYSDATE(),      -- returns date of function completion
+    -- PS_CURRENT_THREAD_ID(),  -- MySQL 8.0.16+
+    UUID_SHORT(),   -- integer
+    UUID()          -- alnum
 ;
