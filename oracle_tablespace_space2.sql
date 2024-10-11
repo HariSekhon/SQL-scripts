90fc8fab2f10106b1cd88b57c8b8f98048aab806
Hari Sekhon <harisekhon@gmail.com>
2024-10-11 03:53:06 +0300

    added oracle_tablespace_space2.sql

---
+++ oracle_tablespace_space2.sql
@@ -0,0 +1,16 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2024-10-11 03:24:48 +0300 (Fri, 11 Oct 2024)
+--
+--  vim:ts=4:sts=4:sw=4:et:filetype=sql
+--
+--  https///github.com/HariSekhon/SQL-scripts
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/HariSekhon
+--
+
+-- Oracle
