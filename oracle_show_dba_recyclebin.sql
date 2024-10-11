1ae200a7234df7c9067819c8054a9219c6ac18a5
Hari Sekhon <harisekhon@gmail.com>
2024-10-11 03:50:03 +0300

    added oracle_show_dba_recyclebin.sql

---
+++ oracle_show_dba_recyclebin.sql
@@ -0,0 +1,29 @@
+--
+--  Author: Hari Sekhon
+--  Date: 2024-10-11 03:50:00 +0300 (Fri, 11 Oct 2024)
+--
+--  vim:ts=4:sts=4:sw=4:et:filetype=sql
+--
+--  https://github.com/HariSekhon/SQL-scripts
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/HariSekhon
+--
+
+-- Oracle - Show All Users Recyclebins via DBA Recyclebin
+
+SELECT
+    username,
+    original_name,
+    type,
+    drop_time,
+    is_parent,
+    recyclebin_namE
+FROM
+    dba_recyclebin
+ORDER BY
+    username, drop_time
+DESC;
