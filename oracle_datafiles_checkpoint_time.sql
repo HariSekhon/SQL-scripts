c1135b5c1ec64fc6aa1dd07699e5500929a1ce52
Hari Sekhon <harisekhon@gmail.com>
2024-10-12 05:56:03 +0300

    updated oracle_datafiles_checkpoint_time.sql

--- oracle_datafiles_checkpoint_time.sql
+++ oracle_datafiles_checkpoint_time.sql
@@ -15,7 +15,7 @@
 
 -- Oracle Show Checkpoint Dates on DBF data files (relevant for DBA recoveries)
 --
--- Tested in late 2000s on Oracle 9i, 10g, 11g and 2024 on Oracle 19c
+-- Tested on Oracle 9i, 10g, 11g, Oracle 19c
 
 
 SELECT
