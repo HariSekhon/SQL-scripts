2d5453c97176ae940363fe2f06c531b08998aff6
Hari Sekhon <harisekhon@gmail.com>
2024-10-12 05:55:55 +0300

    updated oracle_datafiles_lastchanged.sql

--- oracle_datafiles_lastchanged.sql
+++ oracle_datafiles_lastchanged.sql
@@ -13,7 +13,9 @@
 --  https://www.linkedin.com/in/HariSekhon
 --
 
--- Oracle last tested in late 2000s on Oracle 9i - 11
+-- Oracle Show the system change number (SCN)
+--
+-- Tested on Oracle 9i, 10g, 11g, 19c
 
 SELECT
     last_change#,
