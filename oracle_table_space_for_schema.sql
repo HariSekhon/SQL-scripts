5d1e2ee925709e177b0b1d86ad89bb019d24d972
Hari Sekhon <harisekhon@gmail.com>
2024-10-17 05:52:49 +0400

    updated oracle_table_space_for_schema.sql

--- oracle_table_space_for_schema.sql
+++ oracle_table_space_for_schema.sql
@@ -18,30 +18,38 @@
 -- Tested on Oracle 19c
 
 SELECT
-    owner,
-    table_name,
+    t.owner,
+    t.table_name,
     -- each block is 8KB, multiply it to GB, round to two decimal places
-    ROUND(blocks * 8 / 1024 / 1024, 2) AS total_gb,
+    ROUND(t.blocks * 8 / 1024 / 1024, 2) AS total_gb_from_tables,
+    -- estimate from segments
+    ROUND(s.bytes / 1024 / 1024 / 1024, 2) AS total_gb_from_segments,
     -- estimate data size from rows vs average row size, round to two decimal places
-    ROUND(num_rows * avg_row_len / 1024 / 1024 / 1024, 2) AS actual_data_gb,
+    ROUND(t.num_rows * t.avg_row_len / 1024 / 1024 / 1024, 2) AS actual_data_gb,
     -- estimate free space by subtracting the two above calculations
-    ROUND((blocks * 8 / 1024 / 1024) - (num_rows * avg_row_len / 1024 / 1024 / 1024), 2) AS free_space_gb,
+    ROUND(((t.blocks * 8 / 1024) - (t.num_rows * t.avg_row_len / 1024 / 1024)) / 1024, 2) AS free_space_gb,
     -- calculate free space percentage from the above three calculations
     ROUND(
-        ( (blocks * 8 / 1024 / 1024) - (num_rows * avg_row_len / 1024 / 1024 / 1024) ) /
-        (blocks * 8 / 1024 / 1024) * 100, 2) AS free_space_pct
+        ( (t.blocks * 8) - (t.num_rows * t.avg_row_len / 1024) ) /
+        (t.blocks * 8) * 100, 2) AS free_space_pct,
+    t.last_analyzed
 FROM
-    dba_tables
+    dba_tables t
+JOIN
+    dba_segments s
+ON
+    t.owner = s.owner
+        AND
+    t.table_name = s.segment_name
 WHERE
-    blocks > 0
+    t.blocks > 0
         AND
-    num_rows > 0
+    t.num_rows > 0
         AND
-    ((blocks * 8 / 1024 / 1024) - (num_rows * avg_row_len / 1024 / 1024 / 1024)) /
-    (blocks * 8 / 1024 / 1024) > 0.2  -- TUNE: currently only showing tables with over 20% free space
+    -- TUNE: currently only showing tables with over 20% free space
+    ((t.blocks * 8 / 1024) - (t.num_rows * t.avg_row_len / 1024 / 1024)) / (t.blocks * 8 / 1024) > 0.2
         AND
-    owner NOT IN
-      ('SYS', 'SYSTEM', 'SYSAUX', 'RDSADMIN')
+    t.owner = 'USERS'  -- XXX: Change this to your owner schema
 ORDER BY
     free_space_gb DESC,
-    total_gb DESC;
+    total_gb_from_segments DESC;
